ba8f6412d4b766ddcb299216bc06c1fb8b3ad58a
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-02-06 08:37:11 +0200

    Editted the header and added a container for all the components.

--- financeflow.client/src/components/Views/CreateExpense.vue
+++ financeflow.client/src/components/Views/CreateExpense.vue
@@ -1,25 +1,29 @@
 <template>
 
-<ContainerBg>
-  <div class="mx-auto max-w-xl">
-  <form action="" class="space-y-5">
-    <Select id="Category" name="Category" />
-    <Inputbox id="amount" type="Number" placeholder="Price" name="Amount"/>
-    <Inputbox id="quantity" type="Number" placeholder="Quantity" name="Quantity"/>
-    <Textarea id="notes" placeholder="Notes" name="Notes"/>
-    <!-- <div>
+  <ContainerBg>
+    <div class="mx-auto max-w-xl">
+      <div class="text-center mb-8">
+        <h2 class="text-3xl md:text-4xl font-extrabold mb-2">Create an Expense</h2>
+      </div>
+      <form action="" class="space-y-5">
+        <Select id="Category" name="Category" />
+        <Inputbox id="amount" type="Number" placeholder="Price" name="Amount" />
+        <Inputbox id="quantity" type="Number" placeholder="Quantity" name="Quantity" />
+        <Textarea id="notes" placeholder="Notes" name="Notes" />
+        <!-- <div>
       <label for="example2" class="mb-1 block text-sm font-medium text-gray-700">Password</label>
       <input type="password" id="example2" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500" placeholder="Password" />
     </div> -->
-    <div class="flex items-center space-x-2">
-      <input type="checkbox" id="example3" class="h-4 w-4 rounded border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400" />
-      <label for="example3" class="text-sm font-medium text-gray-700">Remember me</label>
+        <!-- <div class="flex items-center space-x-2">
+          <input type="checkbox" id="example3"
+            class="h-4 w-4 rounded border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400" />
+          <label for="example3" class="text-sm font-medium text-gray-700">Remember me</label>
+        </div> -->
+        <Button name="Create" />
+      </form>
     </div>
-    <Button name="Create"/>
-  </form>
-</div>
 
-</ContainerBg>
+  </ContainerBg>
 
 
 </template>
@@ -32,6 +36,4 @@ import Select from '../Components/Forms/Select.vue';
 import ContainerBg from '../Components/ContainerBg.vue';
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
