600bf2864e4915ae4e92edbb2dea631a2ed59c0e
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-02-06 13:37:28 +0200

    Added a new card for the dashboard cards.

--- financeflow.client/src/App.vue
+++ financeflow.client/src/App.vue
@@ -43,18 +43,18 @@
     <div class="flex flex-col flex-grow">
       <div class="flex items-center flex-shrink-0 h-16 px-8 border-b border-gray-300">
         <h1 class="text-lg font-medium">Dashboard</h1>
-        <!-- <button class="flex items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-300">
+        <button class="flex items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-300">
           Action 1
-        </button> -->
-        <button class="flex items-center justify-center h-10 px-4 ml-2 text-sm font-medium rounded hover:bg-gray-300">
+        </button>
+        <button class="flex items-center justify-center h-10 px-4 ml-2 text-sm font-medium rounded-full hover:bg-gray-300">
           <BellIcon class="w-5 h-5" />
         </button>
         <button class="relative ml-2 text-sm focus:outline-none group">
-          <div class="flex items-center justify-between w-20 h-10 rounded hover:bg-gray-300 bg-gray-200">
+          <div class="flex items-center justify-between w-10 h-10 rounded-full hover:bg-gray-300 bg-gray-200">
             <!-- <svg class="w-5 h-5 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
             </svg> -->
-            <span class="px-4 py-2">Martin</span>
+            <!-- <span class="px-4 py-2">Martin</span> -->
           </div>
           <div class="absolute right-0 flex-col items-start hidden w-40 pb-1 rounded pt-2 bg-white border border-gray-300 shadow-lg group-focus:flex">
             <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
--- financeflow.client/src/components/Views/index.vue
+++ financeflow.client/src/components/Views/index.vue
@@ -1,9 +1,10 @@
 <template>
   <main>
-    <div class="grid grid-cols-3 gap-6">
-      <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-      <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-      <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
+    <div class="grid grid-cols-4 gap-5">
+      <DashMincard title="Balance" subtitle="Your Balance is "/>
+      <DashMincard title="Monthly Income" subtitle="Your Monthly Income is "/>
+      <DashMincard title="Nonthly Expenses" subtitle="Your Monthly Expenses are "/>
+      <DashMincard title="Monthly Budget" subtitle="Your Monthly Budget is "/>
       <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
       <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
       <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
@@ -30,6 +31,8 @@
 </template>
 
 <script setup>
+import DashMincard from '../Components/DashMincard.vue';
+
 
 </script>
 
