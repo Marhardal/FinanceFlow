0d1527b7d450a3f5d5c1762dadecc7914d45d582
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-02-21 08:31:23 +0200

    Atims can be now be added successfull. There is need to add a tost notifications which would be helpful for endusers to see if the items have been added or not.

--- financeflow.client/src/components/Views/CreateItem.vue
+++ financeflow.client/src/components/Views/CreateItem.vue
@@ -6,7 +6,7 @@
         <h2 class="text-2xl md:text-2xl text-neutral-600 font-semibold mb-2">Fill in all Fields.</h2>
       </div>
       <!-- <form action="" class="space-y-5"> -->
-      <FormKit type="form" submit-label="Create" @submit="login" :submit-attrs="{
+      <FormKit type="form" submit-label="Create" @submit="createItem" :submit-attrs="{
     inputClass: 'py-3 px-4 block w-full border-gray-500 rounded text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none bg-gray-300 dark:border-gray-500 dark:text-neutral-700 dark:placeholder-neutral-500 dark:focus:ring-neutral-600',
     wrapperClass: 'space-y-5',
     'data-theme': `dark`,
@@ -14,7 +14,7 @@
   }">
         <FormKit label="Item Name" placeholder="Enter an Item Name." type="text" help="Enter an Item Name."
           value="" v-model="input.name"/>
-        <FormKit type="select" label="Select Item Category" :options="Categories" v-model="input.category" help="Select an Item Category" />
+        <FormKit type="select" label="Select Item Category" :options="Categories" v-model="input.expenseCategoryId" help="Select an Item Category" />
         <FormKit label="Item Price" placeholder="Enter an Item Price." type="number" v-model="input.price" help="Enter an Item Name."
           value="" />
       </FormKit>
@@ -42,11 +42,20 @@ const getCategories = async () => {
   }
 };
 
+
+const createItem = async () => {
+  try {
+    const response = await apiClient.post('Item', input);
+    router.push({ name: 'Items' });
+  } catch (error) {
+    // console.log(input);
+    console.error("Error creating Item:", error);
+  }
+}
 const input = reactive({
   name,
-  price: '',
-  measurement: '',
-  category: '',
+  price: 0,
+  expenseCategoryId: '',
 });
 
 
