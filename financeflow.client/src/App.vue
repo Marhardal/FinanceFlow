44f2a23aa8b86b4b5fb8c6064ab0b9a36000c6b5
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-02-23 00:17:09 +0200

    Update header title to reflect finance context
    Changed the title in the header from "Dashboard" to "Finance Flow" for better alignment with the application's focus on financial management.

--- financeflow.client/src/App.vue
+++ financeflow.client/src/App.vue
@@ -42,7 +42,7 @@
 
     <div class="flex flex-col flex-grow">
       <div class="flex items-center flex-shrink-0 h-16 px-8 border-b border-gray-300">
-        <h1 class="text-lg font-medium">Dashboard</h1>
+        <h1 class="text-lg font-medium">Finance Flow</h1>
         <button class="flex items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-300">
           Action 1
         </button>
@@ -51,6 +51,7 @@
         </button>
         <button class="relative ml-2 text-sm focus:outline-none group">
           <div class="flex items-center justify-between w-10 h-10 rounded-full hover:bg-gray-300 bg-gray-200">
+            <UserCircleIcon class="icon size-10" />
             <!-- <svg class="w-5 h-5 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
             </svg> -->
