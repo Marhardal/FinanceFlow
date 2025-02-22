27f345a9537acd3174fefa3e012bfa04cd6ced23
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-02-23 00:03:04 +0200

    Refactor ListHeader layout and search functionality
    Updated `ListHeader.vue` to use a grid layout for better organization of elements. Moved the search input into `ListHeader`, enhancing encapsulation. In `item.vue`, replaced the commented-out `FormKit` search component with a new search input integrated into `ListHeader`, preserving functionality while improving code structure.

--- financeflow.client/src/components/Components/ListHeader.vue
+++ financeflow.client/src/components/Components/ListHeader.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class=" flex items-center justify-between pb-6">
-    <div>
+  <div class="grid grid-cols-12 gap-2 items-center justify-between pb-6">
+    <div class="col-span-6">
       <h2 class="text-gray-600 font-semibold">{{ props.Header }}</h2>
       <span class="text-xs">{{ props.SubHeader }}</span>
     </div>
-    <div class="flex items-center justify-between">
-      <div class="flex bg-gray-50 items-center p-2 rounded-md outline-2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd"
-            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-            clip-rule="evenodd" />
-        </svg>
-        <input class="bg-gray-50 ml-1 block outline-none flex-grow" type="text" name="" id="" placeholder="search...">
+    <div class="col-span-6 flex justify-end">
+
+      <div class="grid grid-cols-6 gap-2">
+      <div class="col-span-5">
+        <slot></slot>
       </div>
-      <div class="lg:ml-40 ml-10 space-x-8">
-        <router-link class="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer" :to="props.Navigate">Create</router-link>
+      <div class="col-span-1 space-x-8 py-2">
+        <router-link class="bg-indigo-600 px-4 py-2 my-4 rounded-md text-white font-semibold tracking-wide cursor-pointer" :to="props.Navigate">Create</router-link>
       </div>
     </div>
+      </div>
   </div>
 </template>
 
--- financeflow.client/src/components/Views/item.vue
+++ financeflow.client/src/components/Views/item.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="bg-white p-8 rounded-md w-full">
-    <ListHeader Header="Items" SubHeader="Item List" Navigate="item/create" />
-    <FormKit type="search" label="Search" placeholder="Search for items" v-model="search" @keyup="getSearchedItems()"/>
+    <ListHeader Header="Items" SubHeader="Item List" Navigate="item/create">
+      <!-- <FormKit type="search" label="Search" placeholder="Search for items" v-model="search" @keyup="getSearchedItems()"/> -->
+
+      <div class="flex bg-gray-50 items-center p-2 rounded-md outline-2 w-full">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            clip-rule="evenodd" />
+        </svg>
+        <input class="bg-gray-50 ml-1 block outline-none flex-grow" type="text" name="" id="" placeholder="Search for items"
+          v-model="search" @keyup="getSearchedItems()">
+      </div>
+
+    </ListHeader>
     <div>
       <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div class="flex flex-col">
