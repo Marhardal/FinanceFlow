e024da9b8cc58ffe34fe214314fa88adb6b85214
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-02-23 07:44:05 +0200

    Add IncomeModel class for financial data representation
    Introduces the `IncomeModel` class in the `FinanceFlow.Server.Models` namespace. This class includes properties for `id`, `name`, `StatusID`, `description`, `amount`, and `createdate`, with appropriate data annotations for validation and database mapping. The `amount` property has a precision of 18 and scale of 2, while `createdat` is initialized to the current date and time. The class also establishes a relationship with `StatusModel`, featuring a nullable `Status` property and a collection of `Statuses`.

---
+++ FinanceFlow.Server/Models/IncomeModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceFlow.Server.Models
+{
+    public class IncomeModel
+    {
+        public int id { get; set; }
+
+        public required string name { get; set; }
+
+        [ForeignKey("StatusID")]
+        public int StatusID { get; set; }
+
+        public required string description { get; set; }
+
+        [Precision(18, 2)]
+        public required decimal amount { get; set; }
+
+        public required DateTime createdate { get; set; } = DateTime.Now;
+
+        public StatusModel? Status { get; set; }
+
+        public ICollection<StatusModel> Statuses { get; set; } = new List<StatusModel>();
+    }
+}
