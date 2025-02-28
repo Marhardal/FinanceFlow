8f9454cc813bac30963ce9bb6f26a761c0dc1daa
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-02-28 05:00:18 +0200

    Added Expense Controller

---
+++ FinanceFlow.Server/Controllers/ExpenseController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanceFlow.Server.DBContext;
+using FinanceFlow.Server.Models;
+
+namespace FinanceFlow.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly FinanceDBContext _context;
+
+        public ExpenseController(FinanceDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Expense
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExpenseModel>>> GetExpenses()
+        {
+            return await _context.Expenses.ToListAsync();
+        }
+
+        // GET: api/Expense/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExpenseModel>> GetExpenseModel(int id)
+        {
+            var expenseModel = await _context.Expenses.FindAsync(id);
+
+            if (expenseModel == null)
+            {
+                return NotFound();
+            }
+
+            return expenseModel;
+        }
+
+        // PUT: api/Expense/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExpenseModel(int id, ExpenseModel expenseModel)
+        {
+            if (id != expenseModel.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(expenseModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExpenseModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Expense
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ExpenseModel>> PostExpenseModel(ExpenseModel expenseModel)
+        {
+            _context.Expenses.Add(expenseModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetExpenseModel", new { id = expenseModel.id }, expenseModel);
+        }
+
+        // DELETE: api/Expense/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpenseModel(int id)
+        {
+            var expenseModel = await _context.Expenses.FindAsync(id);
+            if (expenseModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Expenses.Remove(expenseModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ExpenseModelExists(int id)
+        {
+            return _context.Expenses.Any(e => e.id == id);
+        }
+    }
+}
