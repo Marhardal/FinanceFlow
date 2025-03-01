c0c392660706588a150f9e441ee70140a212b9ba
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-03-01 06:24:25 +0200

    Refactor budget management UI and model properties
    - Commented out the unused `Budgets` property in `StatusModel.cs`.
    - Added a search input field in `Budgets.vue` for filtering budgets.
    - Introduced a new "Income" column in the budget table.
    - Renamed `Amount` to `amount` and replaced `date` with `remindon`.
    - Implemented a reactive `search` variable and `getSearchedBudgets` method for enhanced functionality.

--- financeflow.client/src/components/Views/Budgets.vue
+++ financeflow.client/src/components/Views/Budgets.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="bg-white p-8 rounded-md w-full">
-    <ListHeader Header="Budgets" SubHeader="Budget List" Navigate="Budget/create" />
+    <ListHeader Header="Budgets" SubHeader="Budget List" Navigate="Budget/create">
+      <div class="flex bg-gray-50 items-center p-2 rounded-md outline-2 w-full">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            clip-rule="evenodd" />
+        </svg>
+        <input class="bg-gray-50 ml-1 block outline-none flex-grow" type="text" name="" id=""
+          placeholder="Search for a Budget" v-model="search" @keyup="getSearchedBudgets()">
+      </div>
+    </ListHeader>
     <div>
       <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div class="flex flex-col">
@@ -16,6 +26,9 @@
                       <th scope="col"
                         class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
                         Status</th>
+                        <th scope="col"
+                        class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                        Income</th>
                       <th scope="col"
                         class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
                         Amount</th>
@@ -35,9 +48,11 @@
                         {{ Budget.name }}</td>
                       <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 ">
                         {{ Budget.status.name }}</td>
-                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{ Budget.Amount }}</td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 ">
+                        {{ Budget.income.name }}</td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{ Budget.amount }}</td>
                       <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">
-                        {{ Budget.date }}</td>
+                        {{ Budget.remindon }}</td>
                       <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
                         <router-link :to="{path: 'budget/edit/' + Budget.id}"
                           class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent pr-1 text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">Edit
@@ -84,6 +99,13 @@ import apiClient from '../../Others/apiClient';
 import { onMounted, ref } from 'vue';
 
 const Budgets = ref([]);
+
+const search = ref('');
+
+const getSearchedBudgets = async () => {
+  const response = await apiClient.get(`budgets?search=${search.value}`);
+  Budgets.value = response.data;
+};
 const getBudgets = async () => {
   const response = await apiClient.get('budgets');
   Budgets.value = response.data;
