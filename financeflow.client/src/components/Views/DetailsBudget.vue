ba948ad141f0ab311635f8291cc57605657969b1
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-03-02 11:05:11 +0200

    Enhance budget details with expense list functionality
    Updated `DetailsBudget.vue` to include a new expense list section with a table for displaying expenses. Adjusted the budget details header font size and added buttons for downloading and creating expenses. Implemented action buttons for editing, viewing details, and deleting entries. Added a message for cases with no expenses found. Included a console log for budget income details in the script section.

--- financeflow.client/src/components/Views/DetailsBudget.vue
+++ financeflow.client/src/components/Views/DetailsBudget.vue
@@ -3,7 +3,7 @@
   <ContainerBg>
     <div v-if="Budget">
       <div class="border-b border-gray-200 p-5 rounded-lg">
-        <h2 class="text-3xl md:text-4xl font-bold mb-2">{{ Budget.name }} Details</h2>
+        <h2 class="text-3xl md:text-2xl font-bold mb-2">{{ Budget.name }} Details</h2>
       </div>
       <div class="border-b border-gray-200 p-5 rounded-lg">
         <div class="grid grid-cols-4 gap-4">
@@ -22,13 +22,13 @@
           <div class="col-span-1">
             <h3 class="text-lg font-semibold mb-3">Budgeted Income</h3>
           </div>
-          <div class="col-span-3">
+          <div class="col-span-3" v-if="Budget.income">
             <p class="text-md">{{ Budget.income.name + "->" + Budget.income.amount }}</p>
           </div>
           <div class="col-span-1">
             <h3 class="text-lg font-semibold mb-3">Budgeted Status</h3>
           </div>
-          <div class="col-span-3">
+          <div class="col-span-3" v-if="Budget.status">
             <p class="text-md">{{ Budget.status.name }}</p>
           </div>
           <div class="col-span-1">
@@ -48,6 +48,97 @@
           </div>
           <div class="col-span-3">
             <h3 class="text-md">{{ Budget.description }}</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class=" py-4">
+        <div class="grid grid-cols-7 border-b border-gray-200">
+          <div class="col-span-5">
+            <h2 class="text-xl py-2 font-bold">Expense List</h2>
+          </div>
+          <div class="col-span-1">
+            <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none bg-gray-800 px-4 py-2">Download</button>
+          </div>
+          <div class="col-span-1">
+            <router-link to="" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-white hover:text-black focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none bg-blue-600 px-4 py-2 justify-end align-baseline">Create
+            </router-link>
+          </div>
+        </div>
+        <div>
+          <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto" v-if="Budget.expenses">
+            <div class="flex flex-col">
+              <div class="-m-1.5 overflow-x-auto">
+                <div class="p-1.5 min-w-full inline-block align-middle">
+                  <div class="overflow-hidden">
+                    <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                      <thead>
+                        <tr>
+                          <th scope="col"
+                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                            Item Name</th>
+                          <th scope="col"
+                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                            Amount</th>
+                          <th scope="col"
+                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                            Quantity</th>
+                          <th scope="col"
+                            class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                            Date</th>
+                          <th scope="col"
+                            class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">
+                            Action</th>
+                        </tr>
+                      </thead>
+                      <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+                        <tr class="hover:bg-gray-100 dark:hover:bg-neutral-500 hover:rounded">
+                          <!-- dark:text-neutral-200 -->
+                          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 ">
+                            John Brown</td>
+                          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">45</td>
+                          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">New York No. 1
+                            Lake Park</td>
+                          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 ">New York No. 1
+                            Lake Park</td>
+                          <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                            <router-link to=""
+                              class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent pr-1 text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">Edit
+                              |</router-link>
+                            <router-link to=""
+                              class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent pr-1 text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">Details
+                              |</router-link>
+                            <button type="button"
+                              class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">
+                              Delete</button>
+                          </td>
+                        </tr>
+
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between ">
+              <span class="text-xs xs:text-sm text-gray-900">
+                Showing 1 to 4 of 50 Entries
+              </span>
+              <div class="inline-flex mt-2 xs:mt-0">
+                <button
+                  class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
+                  Prev
+                </button>
+                &nbsp; &nbsp;
+                <button
+                  class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r">
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class=" py-5" v-else>
+            <p class="text-center text-lg font-semibold">No Expenses Found</p>
           </div>
         </div>
       </div>
@@ -72,6 +163,8 @@ const getBudget = async (id) => {
   try {
     const response = await apiClient.get(`Budgets/${id}`);
     Budget.value = response.data;
+
+    console.log(Budget.value.income);
   } catch (error) {
     console.error("Error fetching Budget:", error);
   }
