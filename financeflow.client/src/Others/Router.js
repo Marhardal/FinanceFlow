072b32c5bb4f521e5f63b48bdc98961f28be2956
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-03-21 05:13:23 +0200

    All vue routes are proctected

--- financeflow.client/src/Others/Router.js
+++ financeflow.client/src/Others/Router.js
@@ -19,30 +19,54 @@ import EditExpense from '@/components/Views/EditExpense.vue'
 import Register from '@/components/Views/Register.vue'
 import Login from '@/components/Views/Login.vue'
 
+
+function auth(to, from) {
+  if (!localStorage.getItem('authToken')) {
+      return "/login";
+  }
+}
+
+function guest(to, from) {
+  if (localStorage.getItem('authToken')) {
+      return "/";
+  }
+}
+
 const routes = [
   { path: '/', component: index },
-      { path: '/transactions', component: Transactions },
-      { path: '/incomes', component: Incomes },
-      { path: '/income/create', component: CreateIncome },
-      { path: '/income/edit/:id', component: EditIncome },
-      { path: '/income/details/:id', component: DetailsIncome },
-      { path: '/expenses', component: Expenses },
-      { path: '/Budget/:id/expense/create', component: CreateExpense },
-      { path: '/Budget/:budgetid/expense/edit/:expenseid', component: EditExpense },
-      { path: '/items', component: Item },
-      { path: '/item/Create', component: CreateItem },
-      { path: '/item/edit/:id', component: EditItem },
-      { path: '/Budgets', component: Budgets },
-      { path: '/Budget/create', component: CreateBudget },
-      { path: '/Budget/edit/:id', component: EditBudget },
-      { path: '/Budget/details/:id', component: DetailsBudget },
-      { path: '/register', component: Register},
-      { path: '/login', component: Login},
+      { path: '/transactions', component: Transactions, beforeEnter: auth },
+      { path: '/incomes', component: Incomes, beforeEnter: auth },
+      { path: '/income/create', component: CreateIncome, beforeEnter: auth },
+      { path: '/income/edit/:id', component: EditIncome, beforeEnter: auth },
+      { path: '/income/details/:id', component: DetailsIncome,  beforeEnter: auth },
+      { path: '/expenses', component: Expenses, beforeEnter: auth },
+      { path: '/Budget/:id/expense/create', component: CreateExpense, beforeEnter: auth },
+      { path: '/Budget/:budgetid/expense/edit/:expenseid', component: EditExpense, beforeEnter: auth },
+      { path: '/items', component: Item, beforeEnter: auth },
+      { path: '/item/Create', component: CreateItem, beforeEnter: auth },
+      { path: '/item/edit/:id', component: EditItem, beforeEnter: auth },
+      { path: '/Budgets', component: Budgets,  beforeEnter: auth },
+      { path: '/Budget/create', component: CreateBudget, beforeEnter: auth },
+      { path: '/Budget/edit/:id', component: EditBudget, beforeEnter: auth },
+      { path: '/Budget/details/:id', component: DetailsBudget, beforeEnter: auth },
+      { path: '/register', component: Register, beforeEnter: guest},
+      { path: '/login', component: Login, beforeEnter: guest},
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+// router.beforeEach((to, from, next) => {
+//   const token = localStorage.getItem("authToken") // Get the token from storage (localStorage/cookies)
+
+//   if (to.name !== 'login' && !token) {
+//     router.push('login') // Redirect to login if not authenticated
+//   } else {
+//     next() // Allow navigation
+//   }
+// })
+
 
 export default router;
