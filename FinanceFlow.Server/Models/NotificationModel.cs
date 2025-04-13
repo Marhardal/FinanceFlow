cbea5bf6bbf1a8faf076ac58ab73cb58a53afe94
Martin Harawa <114500768+Marhardal@users.noreply.github.com>
2025-04-13 21:04:58 +0200

    Designed the notification model.

---
+++ FinanceFlow.Server/Models/NotificationModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceFlow.Server.Models
+{
+    public class NotificationModel
+    {
+        public int Id { get; set; }
+
+        public NotifyType Type { get; set; }
+
+        [ForeignKey("IncomeID")]
+        public int IncomeID { get; set; }
+
+        [ForeignKey("BudgetID")]
+        public int BudgetID { get; set; }
+
+        public string? Message { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime createdOn { get; set; } = DateTime.UtcNow;
+
+        public IncomeModel? Income { get; set; }
+
+        public BudgetModel? Budget { get; set; }
+        public enum NotifyType
+        {
+            Income,
+            Budget,
+        }
+    }
+}
